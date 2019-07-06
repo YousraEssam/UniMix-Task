3d4376f72df8f98180508719c71e29c2a857832e
YousraEssam <yousraa.essam.15@hotmail.com>
2019-07-06 05:32:33 +0200

    view all photos

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Photo;
 
 class PhotosController extends Controller
 {
@@ -13,7 +14,10 @@ class PhotosController extends Controller
      */
     public function index()
     {
-        //
+        $photos = Photo::with('product')->get();
+        return view('photos.index',[
+            'photos' => $photos,
+        ]);
     }
 
     /**
---
+++ resources/views/photos/index.blade.php
@@ -0,0 +1,50 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+<h1 style="text-align:center; color:brown; font-size:28px; font-weight:bold;">Photos</h1>
+@stop
+
+@section('content')
+
+    <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Photo Name</th>
+                <th scope="col">Is Logo ?</th>
+                <th scope="col">to Product</th>
+                <th scope="col">#</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($photos as $photo)
+                    
+                <tr>
+                    <th scope="row">{{ $photo->id }}</th>
+                    <td>{{ $photo->photo_name }}</td>
+                    <td>{{ $photo->logo }}</td>
+                    <td>{{ $photo->product->product_name }}</td>
+                    <td>
+                        <a href="{{route('photos.show', [$photo->id])}}" class="btn btn-success">View</a>
+                        <a href="{{route('photos.edit', [$photo->id])}}" class="btn btn-success">Edit</a>
+                        <form action="{{route('photos.destroy', [$photo->id])}}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button class="btn btn-danger" onclick="return confirm('Are you sure?')"> Delete </button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+          </table>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
