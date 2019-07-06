bf4585ea32dfd76183251b90cf615794d4036558
YousraEssam <yousraa.essam.15@hotmail.com>
2019-07-06 05:33:42 +0200

    remove extra comments

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,20 +7,16 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel">
                 <div class="pull-left image">
-                    {{-- <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image"> --}}
                     <img src="images/admin.png" class="img-circle" alt="User Image">
 
                 </div>
                 <div class="pull-left info">
                     <p>{{Auth::User()->name}}</p>
                     <!-- Status -->
-                    <!-- <a href="#"><i class="fa fa-circle text-success"></i> Online</a> -->
+                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
                 </div>
             </div>
-    
-            
-            <!-- /.search form -->
-    
+
             <!-- Sidebar Menu -->
             <ul class="sidebar-menu" data-widget="tree">
             <li class="header"> </li>
