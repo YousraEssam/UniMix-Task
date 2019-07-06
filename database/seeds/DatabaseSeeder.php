0bf1142b14e2c4dbfb048f3dce600a4cc34954ad
YousraEssam <yousraa.essam.15@hotmail.com>
2019-07-06 02:37:57 +0200

    add user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             ProductsTableSeeder::class,
             PhotosTableSeeder::class,
             InformationTableSeeder::class,
+            UsersTableSeeder::class,
         ]);
     }
 }
